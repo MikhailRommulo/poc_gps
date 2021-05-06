edf46bdb7be6c7774fc868ed0795e198a3e8eeb1
MikhailRommulo <mikhailrommulo@gmail.com>
2021-05-06 16:18:06 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import { Redirect, Route } from 'react-router-dom';
+import { IonApp, IonButton, IonContent, IonLabel, IonRouterOutlet } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { Plugins } from '@capacitor/core';
+import Home from './pages/Home';
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+/* Theme variables */
+import './theme/variables.css';
+import { useState } from 'react';
+
+const { Geolocation } = Plugins;
+
+const App: React.FC = () => {
+  const [position, setPostion] = useState<any>();
+
+  return (
+    <IonApp>
+      <IonContent>
+        <IonButton onClick={async () => {
+          const positionValue = await Geolocation.getCurrentPosition();
+          setPostion({latitude: positionValue.coords.latitude, longitude: positionValue.coords.longitude});
+          }}>
+          <IonLabel>{`${position?.latitude} | ${position?.longitude}`}</IonLabel>
+        </IonButton>
+      </IonContent>
+      {/* <IonReactRouter>
+        <IonRouterOutlet>
+          <Route exact path="/home">
+            <Home />
+          </Route>
+          <Route exact path="/">
+            <Redirect to="/home" />
+          </Route>
+        </IonRouterOutlet>
+      </IonReactRouter> */}
+    </IonApp>
+  )
+};
+
+export default App;
